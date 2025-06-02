7070fb3d9672cd83859de425ae96498ef094dffe
Albert Castillo <Albert.castillom@gmail.com>
2025-06-01 22:28:25 -0700

    working on website

--- Website/webpack.config.js
+++ Website/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { watchFile } = require("fs");
 
 module.exports = {
   mode: "development",
@@ -30,6 +29,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource", // <-- handles images
       },
     ],
   },
