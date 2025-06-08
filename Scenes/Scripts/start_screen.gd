e1175f5f25968c713e1cda289949c23ebed906da
Daniel Rivera <diegolax@outlook.com>
2025-06-08 15:20:44 -0700

    Fixed decay interval

--- Scenes/Scripts/start_screen.gd
+++ Scenes/Scripts/start_screen.gd
@@ -11,5 +11,5 @@ func _on_start_button_pressed():
 
 func _on_speedrun_button_pressed():
 	Global.is_speedrun = true
-	Global.decay_interval = .5
+	Global.decay_interval = 5.0
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
